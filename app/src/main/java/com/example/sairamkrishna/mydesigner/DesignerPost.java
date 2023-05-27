5aa89cc23c5bca8eab6655ad942a4782b4fb25c0
FaresAlharbi <2040846@uj.edu.sa>
2023-05-28 00:26:03 +0300

    fares

---
+++ app/src/main/java/com/example/sairamkrishna/mydesigner/DesignerPost.java
@@ -0,0 +1,14 @@
+package com.example.sairamkrishna.mydesigner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DesignerPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_designer_post);
+    }
+}
---
+++ app/src/main/java/com/example/sairamkrishna/mydesigner/requestPage.java
@@ -0,0 +1,14 @@
+package com.example.sairamkrishna.mydesigner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class requestPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_page);
+    }
+}
